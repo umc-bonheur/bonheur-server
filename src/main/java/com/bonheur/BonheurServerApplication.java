266a8a03567de368bcffae7a42665598ef1fbda7
Torres-09 <dla612@naver.com>
2023-01-06 01:01:55 +0900

    init

---
+++ src/main/java/com/bonheur/BonheurServerApplication.java
@@ -0,0 +1,13 @@
+package com.bonheur;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BonheurServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BonheurServerApplication.class, args);
+	}
+
+}
