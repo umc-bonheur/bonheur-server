facc0ce523015beb458e6409941aa206702cf460
Torres-09 <dla612@naver.com>
2023-01-06 01:44:44 +0900

    setting: RestControllerAdvice 추가
    예외처리는 해당 클래스에서 전역으로 처리한다.

---
+++ src/main/java/com/bonheur/config/advice/ControllerExceptionAdvice.java
@@ -0,0 +1,12 @@
+package com.bonheur.config.advice;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RequiredArgsConstructor
+@RestControllerAdvice
+public class ControllerExceptionAdvice {
+
+}
