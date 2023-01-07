7508cf012d34419367292a32296368f6459f48eb
hwan2da <dla612@naver.com>
2023-01-08 03:56:45 +0900

    feat: 게시물과 태그의 연관관계 매핑

--- src/main/java/com/bonheur/domain/board/model/Board.java
+++ src/main/java/com/bonheur/domain/board/model/Board.java
@@ -1,11 +1,14 @@
 package com.bonheur.domain.board.model;
 
+import com.bonheur.domain.boardtag.model.BoardTag;
 import com.bonheur.domain.common.BaseEntity;
 import com.bonheur.domain.member.model.Member;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
@@ -18,4 +21,7 @@ public class Board extends BaseEntity {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "id")
     private Member member;
+
+    @OneToMany(mappedBy = "board",cascade = CascadeType.ALL)
+    private List<BoardTag> boardTags = new ArrayList<>();
 }
--- src/main/java/com/bonheur/domain/tag/model/Tag.java
+++ src/main/java/com/bonheur/domain/tag/model/Tag.java
@@ -1,11 +1,15 @@
 package com.bonheur.domain.tag.model;
 
+import com.bonheur.domain.boardtag.model.BoardTag;
 import com.bonheur.domain.common.BaseEntity;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
@@ -13,4 +17,7 @@ import javax.persistence.Entity;
 public class Tag extends BaseEntity {
     @Column(nullable = false, length = 10)
     private String name;
+
+    @OneToMany(mappedBy = "tag")
+    private List<BoardTag> boardTags = new ArrayList<>();
 }
