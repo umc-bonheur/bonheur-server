879b3272b241363a45ba9905d598afeb10990cbf
hwan2da <dla612@naver.com>
2023-01-07 17:35:33 +0900

    feat: 회원 엔티티 추가
    회원은 소셜 정보와 닉네임, 프로필 이미지 링크를 갖는다.
    다른 엔티티 작성에 따라 매핑 관계가 추가된다.

--- src/main/java/com/bonheur/domain/member/model/Member.java
+++ src/main/java/com/bonheur/domain/member/model/Member.java
@@ -1,4 +1,22 @@
 package com.bonheur.domain.member.model;
 
-public class Member {
+import com.bonheur.domain.common.BaseEntity;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class Member extends BaseEntity {
+    @Embedded
+    private MemberSocialInfo memberSocialInfo;
+
+    @Column(length = 30)
+    private String nickname;
+
+    private String profileImage;
 }
