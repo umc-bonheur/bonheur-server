13f0edd253de4c39f235e3d57413cefdeabe72cb
hwan2da <dla612@naver.com>
2023-01-08 03:04:21 +0900

    feat: 태그 엔티티 추가
    name 10글자 제한

--- src/main/java/com/bonheur/domain/tag/model/Tag.java
+++ src/main/java/com/bonheur/domain/tag/model/Tag.java
@@ -1,4 +1,16 @@
 package com.bonheur.domain.tag.model;
 
-public class Tag {
+import com.bonheur.domain.common.BaseEntity;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class Tag extends BaseEntity {
+    @Column(nullable = false, length = 10)
+    private String name;
 }
