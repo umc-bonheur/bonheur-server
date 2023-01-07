7705e068c1d7fabd922e96227a59a2f9aef6f71d
hwan2da <dla612@naver.com>
2023-01-08 00:14:47 +0900

    feat: 게시물 엔티티 추가

--- src/main/java/com/bonheur/domain/board/model/Board.java
+++ src/main/java/com/bonheur/domain/board/model/Board.java
@@ -1,4 +1,17 @@
 package com.bonheur.domain.board.model;
 
-public class Board {
+import com.bonheur.domain.common.BaseEntity;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class Board extends BaseEntity {
+
+    @Column(length = 1000)
+    private String contents;
 }
