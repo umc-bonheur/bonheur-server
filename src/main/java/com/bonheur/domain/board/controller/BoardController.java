9375d9701b55261c2ee33a7cb0a60648e9e22e71
Torres-09 <dla612@naver.com>
2023-01-08 20:43:26 +0900

    setting: BoardController

--- src/main/java/com/bonheur/domain/board/controller/BoardController.java
+++ src/main/java/com/bonheur/domain/board/controller/BoardController.java
@@ -1,4 +1,11 @@
 package com.bonheur.domain.board.controller;
 
+import com.bonheur.domain.board.service.BoardService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
 public class BoardController {
+    private final BoardService boardService;
 }
