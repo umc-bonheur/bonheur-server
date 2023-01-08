314cd12da71cf70a79881c346bfe5fe22dbc2ccf
Torres-09 <dla612@naver.com>
2023-01-08 20:50:40 +0900

    setting: MemberServiceImpl

---
+++ src/main/java/com/bonheur/domain/member/service/MemberServiceImpl.java
@@ -0,0 +1,11 @@
+package com.bonheur.domain.member.service;
+
+import com.bonheur.domain.member.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class MemberServiceImpl implements MemberService{
+    private final MemberRepository memberRepository;
+}
