381a785e410c0ed044b0c3bc1baa802a6b6f79fe
Torres-09 <dla612@naver.com>
2023-01-08 20:43:51 +0900

    setting: BoardRepository

--- src/main/java/com/bonheur/domain/board/repository/BoardRepository.java
+++ src/main/java/com/bonheur/domain/board/repository/BoardRepository.java
@@ -1,4 +1,7 @@
 package com.bonheur.domain.board.repository;
 
-public interface BoardRepository {
+import com.bonheur.domain.board.model.Board;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BoardRepository extends JpaRepository<Board, Long>, BoardRepositoryCustom {
 }
