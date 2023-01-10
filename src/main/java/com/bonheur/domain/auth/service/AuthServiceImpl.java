603a35b6ce82aa040ecf4aa204518581ffbbcf07
Torres-09 <dla612@naver.com>
2023-01-10 23:17:17 +0900

    feat: 소셜 회원가입 로직 추가
    AuthService의 signup 로직에는 transactional 어노테이션을 추가하지 않았다.
    memberservice 에는 트랜잭션 어노테이션이 추가되어있고, 그전에는 DB 접근을 하지 않기 때문에 명시하지 않았다.

---
+++ src/main/java/com/bonheur/domain/auth/service/AuthServiceImpl.java
@@ -0,0 +1,23 @@
+package com.bonheur.domain.auth.service;
+
+import com.bonheur.config.provider.AuthProvider;
+import com.bonheur.domain.auth.model.dto.SocialSignUpRequest;
+import com.bonheur.domain.member.repository.MemberRepository;
+import com.bonheur.domain.member.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class AuthServiceImpl implements AuthService {
+
+    private final MemberRepository memberRepository;
+    private final AuthProviderFinder authProviderFinder;
+    private final MemberService memberService;
+    @Override
+    public Long signUp(SocialSignUpRequest request) {
+        AuthProvider authProvider = authProviderFinder.findAuthProvider(request.getSocialType());
+        String socialId = authProvider.getSocialId(request.getToken());
+        return memberService.registerMember(request.toCreateMemberRequest(socialId));
+    }
+}
