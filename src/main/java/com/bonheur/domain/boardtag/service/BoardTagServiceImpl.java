40243cab49044fff3264428506a59e247e58b869
hwan2da <dla612@naver.com>
2023-01-10 19:22:29 +0900

    setting : local 작업 환경 구축
    h2 데이터베이스 이용

--- src/main/java/com/bonheur/domain/boardtag/service/BoardTagServiceImpl.java
+++ src/main/java/com/bonheur/domain/boardtag/service/BoardTagServiceImpl.java
@@ -1,10 +1,11 @@
 package com.bonheur.domain.boardtag.service;
 
+import com.bonheur.domain.boardtag.repository.BoardTagRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class BoardTagServiceImpl implements BoardTagService{
-    private final BoardTagService boardTagService;
+    private final BoardTagRepository boardTagRepository;
 }
