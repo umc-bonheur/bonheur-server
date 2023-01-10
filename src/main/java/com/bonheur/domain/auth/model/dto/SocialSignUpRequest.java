74ce9914af7c2d654336c88ffd69f36d496c7ea0
Torres-09 <dla612@naver.com>
2023-01-10 23:13:40 +0900

    fix : SignUpRequest -> SocialSignUpRequest 네이밍 변경
    후에 이메일 회원가입 기능 추가 가능성을 위해 분류한다.

--- src/main/java/com/bonheur/domain/auth/model/dto/SocialSignUpRequest.java
+++ src/main/java/com/bonheur/domain/auth/model/dto/SocialSignUpRequest.java
@@ -10,7 +10,7 @@ import lombok.ToString;
 @ToString
 @Getter
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
-public class SignUpRequest {
+public class SocialSignUpRequest {
     private String token;
 
     private String nickname;
