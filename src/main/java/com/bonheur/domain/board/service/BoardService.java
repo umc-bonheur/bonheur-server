faba10321013709ac585c3296d61951428693b7b
m2nja201 <m2nja201@gmail.com>
2023-01-13 23:36:47 +0900

    docs : Board Service 로직 수정 + BoardTag를 String List로 받아오는 함수 구현 : `getBoardTagsName` + memberId에 따른 페이징

--- src/main/java/com/bonheur/domain/board/service/BoardService.java
+++ src/main/java/com/bonheur/domain/board/service/BoardService.java
@@ -1,6 +1,8 @@
 package com.bonheur.domain.board.service;
 
+import com.bonheur.domain.board.model.Board;
 import com.bonheur.domain.board.model.GetBoardResponse;
+import com.bonheur.domain.boardtag.model.BoardTag;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
@@ -9,5 +11,6 @@ import java.util.List;
 
 @Service
 public interface BoardService {
-    public List<GetBoardResponse> getAllBoards(Pageable pageable);
+    public List<GetBoardResponse> getAllBoards(Long memberId, Pageable pageable);
+    public List<String> getBoardTagsName(List<BoardTag> boardTags);
 }
