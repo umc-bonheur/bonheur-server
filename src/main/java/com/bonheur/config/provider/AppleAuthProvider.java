2d41f69806a70436dab87f80690500e766ac8aa1
Torres-09 <dla612@naver.com>
2023-01-14 15:45:01 +0900

    feat: 애플 소셜 ID 가져오기 로직 추가

--- src/main/java/com/bonheur/config/provider/AppleAuthProvider.java
+++ src/main/java/com/bonheur/config/provider/AppleAuthProvider.java
@@ -1,13 +1,15 @@
 package com.bonheur.config.provider;
 
+import com.bonheur.config.client.apple.AppleTokenDecoder;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @RequiredArgsConstructor
 @Component
 public class AppleAuthProvider implements AuthProvider{
+    private final AppleTokenDecoder appleTokenDecoder;
     @Override
     public String getSocialId(String token) {
-        return null;
+        return appleTokenDecoder.getSocialIdFromToken(token);
     }
 }
