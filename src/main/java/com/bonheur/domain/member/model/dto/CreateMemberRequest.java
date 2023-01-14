8d9259680841036645f6ba15ec0ba56c8d6c4db0
Torres-09 <dla612@naver.com>
2023-01-14 17:12:24 +0900

    fix: 팩토리 메서드 패턴 적용

--- src/main/java/com/bonheur/domain/member/model/dto/CreateMemberRequest.java
+++ src/main/java/com/bonheur/domain/member/model/dto/CreateMemberRequest.java
@@ -18,11 +18,19 @@ public class CreateMemberRequest {
     // 닉네임 조건 체크 어노테이션 이후 추가
     private String nickname;
 
-    @Builder
+    @Builder(access = AccessLevel.PRIVATE)
     private CreateMemberRequest(String socialId, MemberSocialType socialType, String nickname) {
         this.socialId = socialId;
         this.socialType = socialType;
         this.nickname = nickname;
+    }
+
+    public static CreateMemberRequest of(String socialId, MemberSocialType socialType, String nickname) {
+        return CreateMemberRequest.builder()
+                .socialId(socialId)
+                .socialType(socialType)
+                .nickname(nickname)
+                .build();
     }
 
     public Member toEntity() {
