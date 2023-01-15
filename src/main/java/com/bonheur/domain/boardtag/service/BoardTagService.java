2867d75ebe766dadce75b24c74f971a5f247511b
HanDaYeon-coder <hdy78901@naver.com>
2023-01-15 19:05:09 +0900

    fix : board tag 생성 기능 구현 로직 수정

--- src/main/java/com/bonheur/domain/boardtag/service/BoardTagService.java
+++ src/main/java/com/bonheur/domain/boardtag/service/BoardTagService.java
@@ -1,4 +1,9 @@
 package com.bonheur.domain.boardtag.service;
+import com.bonheur.domain.board.model.Board;
+import com.bonheur.domain.tag.model.Tag;
+
+import java.util.List;
 
 public interface BoardTagService {
+    List<Long> createBoardTags(Board board, List<Tag> tags);
 }
--- src/main/java/com/bonheur/domain/boardtag/service/BoardTagServiceImpl.java
+++ src/main/java/com/bonheur/domain/boardtag/service/BoardTagServiceImpl.java
@@ -1,11 +1,27 @@
 package com.bonheur.domain.boardtag.service;
 
+import com.bonheur.domain.board.model.Board;
+import com.bonheur.domain.boardtag.model.BoardTag;
 import com.bonheur.domain.boardtag.repository.BoardTagRepository;
+import com.bonheur.domain.tag.model.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class BoardTagServiceImpl implements BoardTagService{
     private final BoardTagRepository boardTagRepository;
+
+    @Override
+    @Transactional
+    public List<Long> createBoardTags(Board board, List<Tag> tags){
+        List<Long> boardTagsId = null;
+        for(Tag tag : tags){
+            boardTagsId.add(boardTagRepository.save(BoardTag.newBoardTag(board, tag)).getId());
+        }
+        return boardTagsId;
+    }
 }
