39009e61793f6e6e29e0068ba2dfa3d4e03f70ba
Torres-09 <dla612@naver.com>
2023-01-16 23:35:30 +0900

    feat: WebConfig 추가
    Interceptor, Resolver 등록

--- src/main/java/com/bonheur/config/web/WebConfig.java
+++ src/main/java/com/bonheur/config/web/WebConfig.java
@@ -1,10 +1,28 @@
 package com.bonheur.config.web;
 
+import com.bonheur.config.interceptor.AuthInterceptor;
+import com.bonheur.config.resolver.MemberIdResolver;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+    private final MemberIdResolver memberIdResolver;
+    private final AuthInterceptor authInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(authInterceptor);
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(memberIdResolver);
+    }
 }
