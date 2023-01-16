a37a34bffc5ecb22fea174cc5092e2eb72e24c51
Torres-09 <dla612@naver.com>
2023-01-16 19:17:20 +0900

    feat: WebConfiguration 클래스 추가

---
+++ src/main/java/com/bonheur/config/web/WebConfig.java
@@ -0,0 +1,10 @@
+package com.bonheur.config.web;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@RequiredArgsConstructor
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+}
