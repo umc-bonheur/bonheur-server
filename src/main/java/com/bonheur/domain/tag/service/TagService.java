2bdbb9d5556bcf375b98484dcd2bf1c6d23caf55
m2nja201 <m2nja201@gmail.com>
2023-01-18 15:16:58 +0900

    feat : Tag 삭제기능 추가(Service - deleteTags)

--- src/main/java/com/bonheur/domain/tag/service/TagService.java
+++ src/main/java/com/bonheur/domain/tag/service/TagService.java
@@ -1,4 +1,9 @@
 package com.bonheur.domain.tag.service;
 
+import com.bonheur.domain.board.model.dto.DeleteBoardResponse;
+
+import java.util.List;
+
 public interface TagService {
+    public List<DeleteBoardResponse> deleteTags();
 }
--- src/main/java/com/bonheur/domain/tag/service/TagServiceImpl.java
+++ src/main/java/com/bonheur/domain/tag/service/TagServiceImpl.java
@@ -1,11 +1,46 @@
 package com.bonheur.domain.tag.service;
 
+import com.bonheur.domain.board.model.dto.DeleteBoardResponse;
+import com.bonheur.domain.boardtag.model.BoardTag;
+import com.bonheur.domain.boardtag.repository.BoardTagRepository;
+import com.bonheur.domain.tag.model.Tag;
 import com.bonheur.domain.tag.repository.TagRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class TagServiceImpl implements TagService{
     private final TagRepository tagRepository;
+    private final BoardTagRepository boardTagRepository;
+
+    // 게시글 삭제를 하고 나서 boardTag를 불러와서 tag_id가 없는 거면
+    // 그냥 그 태그를 지우는 걸로
+
+    @Override
+    @Transactional
+    public List<DeleteBoardResponse> deleteTags() {
+        List<Tag> tags = tagRepository.findAll();
+        List<BoardTag> boardTags = boardTagRepository.findAll();
+        List<Long> tagId = new ArrayList<>();
+        List<DeleteBoardResponse> deleteBoardResponses = new ArrayList<>();
+
+        // boardTag에 있는 tag id 받아오기
+        for (BoardTag boardTag : boardTags) {
+            tagId.add(boardTag.getTag().getId());
+        }
+
+        for (Tag tag : tags) {
+            if (boardTagRepository.findAllByTag(tag).isEmpty()) {
+                deleteBoardResponses.add(new DeleteBoardResponse(tag.getName()));
+                tagRepository.delete(tag);
+            }
+        }
+        return deleteBoardResponses;
+    }
+
 }
