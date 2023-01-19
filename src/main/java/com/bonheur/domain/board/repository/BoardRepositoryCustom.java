bcdb68c07fdcaeec9b94b91181c03d0b254a48f8
m2nja201 <m2nja201@gmail.com>
2023-01-20 00:31:04 +0900

    docs : Board 전체 조회 repository에 findAllWithPaging 추가

--- src/main/java/com/bonheur/domain/board/repository/BoardRepositoryCustom.java
+++ src/main/java/com/bonheur/domain/board/repository/BoardRepositoryCustom.java
@@ -1,4 +1,10 @@
 package com.bonheur.domain.board.repository;
 
+import com.bonheur.domain.board.model.Board;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+
 public interface BoardRepositoryCustom {
+    public List<Board> findAllWithPaging(Long memberId, Pageable pageable);
 }
--- src/main/java/com/bonheur/domain/board/repository/BoardRepositoryCustomImpl.java
+++ src/main/java/com/bonheur/domain/board/repository/BoardRepositoryCustomImpl.java
@@ -1,9 +1,27 @@
 package com.bonheur.domain.board.repository;
 
+import com.bonheur.domain.board.model.Board;
+import com.bonheur.domain.board.model.QBoard;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 public class BoardRepositoryCustomImpl implements BoardRepositoryCustom{
     private final JPAQueryFactory queryFactory;
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<Board> findAllWithPaging(Long memberId, Pageable pageable) {
+        QBoard board = QBoard.board;
+
+        return queryFactory.selectFrom(board)
+                .where(board.member.id.eq(memberId))
+                .orderBy(board.updatedAt.desc())
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize()).fetch();
+    }
 }
