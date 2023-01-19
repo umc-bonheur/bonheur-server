8bdf4040b2db49f87561a2acd214a5cd4172ce9c
m2nja201 <m2nja201@gmail.com>
2023-01-20 00:38:48 +0900

    "docs : [피드백] 생성자 접근 private으로 변경"

--- src/main/java/com/bonheur/domain/board/model/dto/GetBoardResponse.java
+++ src/main/java/com/bonheur/domain/board/model/dto/GetBoardResponse.java
@@ -1,5 +1,6 @@
 package com.bonheur.domain.board.model.dto;
 
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -7,8 +8,8 @@ import lombok.NoArgsConstructor;
 import java.util.List;
 
 @Getter
-@NoArgsConstructor
-@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class GetBoardResponse {
     private String contents;
     private List<String> boardTags;
@@ -16,5 +17,8 @@ public class GetBoardResponse {
 
     public static GetBoardResponse of(String contents, List<String> boardTags, String image) {
         return new GetBoardResponse(contents, boardTags, image);
+    }
+    public static GetBoardResponse withoutImage(String contents, List<String> boardTags) {
+        return new GetBoardResponse(contents, boardTags, null);
     }
 }
