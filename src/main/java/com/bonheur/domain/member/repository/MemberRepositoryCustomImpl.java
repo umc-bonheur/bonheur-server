a31be8cf0eeb72fd19e926928806957fbd460c18
zer0silver <duddms0_0@naver.com>
2023-01-24 20:30:37 +0900

    fix: QueryDSL Expression을 사용 - 기존 데이터베이스 함수를 쓰던 방식에서 QueryDSL Expression 사용하여 날짜 연산을 하도록 변경하였습니다.

--- src/main/java/com/bonheur/domain/member/repository/MemberRepositoryCustomImpl.java
+++ src/main/java/com/bonheur/domain/member/repository/MemberRepositoryCustomImpl.java
@@ -4,8 +4,10 @@ import com.bonheur.domain.member.model.Member;
 import com.bonheur.domain.member.model.MemberSocialType;
 import com.bonheur.domain.member.model.dto.*;
 import com.querydsl.core.types.ExpressionUtils;
+import com.querydsl.core.types.Ops;
 import com.querydsl.core.types.Projections;
-import com.querydsl.core.types.dsl.StringTemplate;
+import com.querydsl.core.types.dsl.Expressions;
+import com.querydsl.core.types.dsl.NumberOperation;
 import com.querydsl.jpa.JPAExpressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
@@ -16,7 +18,6 @@ import static com.bonheur.domain.board.model.QBoard.board;
 import static com.bonheur.domain.boardtag.model.QBoardTag.boardTag;
 import static com.bonheur.domain.member.model.QMember.member;
 import static com.bonheur.domain.tag.model.QTag.tag;
-import static com.querydsl.core.types.dsl.Expressions.stringTemplate;
 import static java.util.Objects.isNull;
 
 @RequiredArgsConstructor
@@ -46,12 +47,7 @@ public class MemberRepositoryCustomImpl implements MemberRepositoryCustom {
 
     @Override
     public FindAllActiveResponse findAllActive(Long memberId) {
-
-        /* mysql */
-        StringTemplate toDate = stringTemplate("DATE_FORMAT({0}, '%Y-%m-%d')", board.createdAt);
-
-        /* h2 */
-        // StringTemplate toDate = stringTemplate("FORMATDATETIME({0}, 'yyyy-MM-dd')", board.createdAt);
+        NumberOperation<Integer> toDate = Expressions.numberOperation(Integer.class, Ops.DateTimeOps.DATE, board.createdAt);
 
         return queryFactory
                 .select(Projections.fields(FindAllActiveResponse.class,
@@ -62,8 +58,7 @@ public class MemberRepositoryCustomImpl implements MemberRepositoryCustom {
                                 .from(board)
                                 .where(board.member.id.eq(memberId))
                                 .groupBy(toDate)
-                                ,"countRecordDay")
-                        ))
+                                ,"countRecordDay")))
                 .from(boardTag, board)
                 .where(boardTag.board.member.id.eq(memberId),
                         board.member.id.eq(memberId))
@@ -91,19 +86,12 @@ public class MemberRepositoryCustomImpl implements MemberRepositoryCustom {
 
     @Override
     public Long findByTime(Long memberId, String start, String end) {
-        // todo : 사용할 데이터베이스 문법으로 변경
-
-        /* mysql */
-        StringTemplate toTime = stringTemplate("DATE_FORMAT({0}, '%h')", board.createdAt);
-
-        /* h2 */
-        // StringTemplate toTime = stringTemplate("FORMATDATETIME({0}, 'HH')", board.createdAt);
+        NumberOperation<Integer> toTime = Expressions.numberOperation(Integer.class, Ops.DateTimeOps.HOUR, board.createdAt);
 
         Long result = queryFactory
                 .select(toTime.count())
                 .from(board)
-                .where(board.member.id.eq(memberId),
-                        toTime.between(start, end))
+                .where(board.member.id.eq(memberId))
                 .groupBy(toTime)
                 .distinct()
                 .fetchFirst();
@@ -113,19 +101,13 @@ public class MemberRepositoryCustomImpl implements MemberRepositoryCustom {
 
     @Override
     public Long findNightTime(Long memberId) {
-        // todo : 사용할 데이터베이스 문법으로 변경
-
-        /* mysql */
-        StringTemplate toTime = stringTemplate("DATE_FORMAT({0}, '%h')", board.createdAt);
-
-        /* h2 */
-        // StringTemplate toTime = stringTemplate("FORMATDATETIME({0}, 'HH')", board.createdAt);
+        NumberOperation<Integer> toTime = Expressions.numberOperation(Integer.class, Ops.DateTimeOps.HOUR, board.createdAt);
 
         Long result = queryFactory
                 .select(toTime.count())
                 .from(board)
                 .where(board.member.id.eq(memberId),
-                        toTime.between("20", "25").or(toTime.between("00","01")))
+                        toTime.between(20, 25).or(toTime.between(0,1)))
                 .groupBy(toTime)
                 .distinct()
                 .fetchFirst();
@@ -135,65 +117,51 @@ public class MemberRepositoryCustomImpl implements MemberRepositoryCustom {
 
     @Override
     public List<FindByDayResponse> findByDay(Long memberId){
-        // todo : 사용할 데이터베이스 문법으로 변경
-
-        /* mysql */
-        StringTemplate toDay = stringTemplate("DAYOFWEEK({0})", board.createdAt);
-
-        /* h2 */
-        // StringTemplate toDay = stringTemplate("DAY_OF_WEEK({0})", board.createdAt);
+        NumberOperation<Integer> toDay = Expressions.numberOperation(Integer.class, Ops.DateTimeOps.DAY_OF_WEEK, board.createdAt);
 
         return queryFactory
                 .select(Projections.fields(FindByDayResponse.class,
-                                        toDay.when("1").then("sun")
-                                                .when("2").then("mon")
-                                                .when("3").then("tue")
-                                                .when("4").then("wed")
-                                                .when("5").then("thr")
-                                                .when("6").then("fri")
-                                                .when("7").then("sat")
+                                        toDay.when(1).then("sun")
+                                                .when(2).then("mon")
+                                                .when(3).then("tue")
+                                                .when(4).then("wed")
+                                                .when(5).then("thr")
+                                                .when(6).then("fri")
+                                                .when(7).then("sat")
                                                 .otherwise("기타").max().as("dayOfWeek"),
                                         toDay.count().as("countDay")
                         ))
                 .from(board)
                 .where(board.member.id.eq(memberId))
                 .groupBy(toDay)
-                .orderBy(toDay.asc())
                 .distinct()
                 .fetch();
     }
 
     @Override
     public List<FindByMonthResponse> findByMonth(Long memberId) {
-        // todo : 사용할 데이터베이스 문법으로 변경
-
-        /* mysql */
-        StringTemplate toMonth = stringTemplate("DATE_FORMAT({0}, '%m')", board.createdAt);
-
-        /* h2 */
-        // StringTemplate toMonth = stringTemplate("FORMATDATETIME({0}, 'MM')", board.createdAt);
+        NumberOperation<Integer> toMonth = Expressions.numberOperation(Integer.class, Ops.DateTimeOps.MONTH, board.createdAt);
 
         return queryFactory
                 .select(Projections.fields(FindByMonthResponse.class,
-                        toMonth.when("01").then("jan")
-                                .when("02").then("feb")
-                                .when("03").then("mar")
-                                .when("04").then("apr")
-                                .when("05").then("may")
-                                .when("06").then("jun")
-                                .when("07").then("jul")
-                                .when("08").then("aug")
-                                .when("09").then("sept")
-                                .when("10").then("oct")
-                                .when("11").then("nov")
-                                .when("12").then("dec")
+                        toMonth.when(1).then("jan")
+                                .when(2).then("feb")
+                                .when(3).then("mar")
+                                .when(4).then("apr")
+                                .when(5).then("may")
+                                .when(6).then("jun")
+                                .when(7).then("jul")
+                                .when(8).then("aug")
+                                .when(9).then("sept")
+                                .when(10).then("oct")
+                                .when(11).then("nov")
+                                .when(12).then("dec")
                                 .otherwise("기타").max().as("month"),
                         toMonth.count().as("countMonth")
                 ))
                 .from(board)
                 .where(board.member.id.eq(memberId))
                 .groupBy(toMonth)
-                .orderBy(toMonth.asc())
                 .distinct()
                 .fetch();
     }
