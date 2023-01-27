c2f556b41c29804cb870bc5ebe19a3367ad2994e
HanDaYeon-coder <hdy78901@naver.com>
2023-01-27 22:27:44 +0900

    refactor : BoardTag 관련 코드
    delete함수를 deleteAllInBatch로 변경 및 사용하지 않는 코드 삭제

--- src/main/java/com/bonheur/domain/boardtag/service/BoardTagServiceImpl.java
+++ src/main/java/com/bonheur/domain/boardtag/service/BoardTagServiceImpl.java
@@ -10,7 +10,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -21,24 +20,18 @@ public class BoardTagServiceImpl implements BoardTagService{
     @Transactional
     public void createBoardTags(Board board, List<Long> tagsIds){    //게시글과 해시태그 맵핑(연결)
         for(Long tagId : tagsIds) {
-            Optional<Tag> tag = tagRepository.findById(tagId);
-            tag.orElseThrow(() -> new RuntimeException("존재하지 않은 태그입니다."));
-            boardTagRepository.save(BoardTag.newBoardTag(board, tag.get()));
+            Tag tag = tagRepository.findById(tagId).orElseThrow(() -> new RuntimeException("존재하지 않은 태그입니다."));
+            boardTagRepository.save(BoardTag.newBoardTag(board, tag));
         }
     }
 
     @Override
     @Transactional
     public void updateBoardTags(Board board, List<Long> tagIds){
-        List<BoardTag> boardTags = boardTagRepository.findAllByBoard(board); //기존 게시글 태그들
 
-        if(boardTags != null){
-            for(BoardTag boardTag : boardTags){
-                boardTagRepository.delete(boardTag);    //기존 게시글 태그 삭제
-            }
-        }
+        boardTagRepository.deleteAllInBatch(boardTagRepository.findAllByBoard(board));    //기존 게시글 태그 삭제
 
-        if(tagIds != null) {
+        if(!tagIds.isEmpty()) {
             createBoardTags(board, tagIds);    //게시글 새로운 태그 추가
         }
     }
