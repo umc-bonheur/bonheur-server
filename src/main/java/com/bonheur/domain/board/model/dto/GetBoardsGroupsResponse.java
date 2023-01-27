cb438ff0af96af4300c7d233685c4ef38fdb6b49
m2nja201 <m2nja201@gmail.com>
2023-01-27 11:27:54 +0900

    feat : 날짜별 그룹화 response 추가

--- src/main/java/com/bonheur/domain/board/model/dto/GetBoardsGroupsResponse.java
+++ src/main/java/com/bonheur/domain/board/model/dto/GetBoardsGroupsResponse.java
@@ -6,15 +6,14 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.util.List;
+import java.util.Map;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class GetBoardsGroupsResponse {
-    private String date; // 형식 : 00월 00일 0요일
-    private List<GetBoardsResponse> getBoardsResponses;
-
-    public static GetBoardsGroupsResponse of(String date, List<GetBoardsResponse> getBoardsResponses) {
-        return new GetBoardsGroupsResponse(date, getBoardsResponses);
+    private Map<String, List<GetBoardsResponse>> group;
+    public static GetBoardsGroupsResponse of(Map<String, List<GetBoardsResponse>> group) {
+        return new GetBoardsGroupsResponse(group);
     }
 }
