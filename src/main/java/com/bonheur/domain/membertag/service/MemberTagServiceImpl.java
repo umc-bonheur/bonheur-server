501bcd4b25e0c50c65eef40c91f70266cb5e8553
HanDaYeon-coder <hdy78901@naver.com>
2023-01-27 22:31:14 +0900

    refactor : MemberTag 관련 코드
    for문을 forEach로 변경 및 findMemberTagByTag 함수 추가

--- src/main/java/com/bonheur/domain/membertag/service/MemberTagServiceImpl.java
+++ src/main/java/com/bonheur/domain/membertag/service/MemberTagServiceImpl.java
@@ -4,7 +4,6 @@ import com.bonheur.domain.member.model.Member;
 import com.bonheur.domain.membertag.model.MemberTag;
 import com.bonheur.domain.membertag.repository.MemberTagRepository;
 import com.bonheur.domain.tag.model.Tag;
-import com.bonheur.domain.tag.repository.TagRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -17,20 +16,19 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class MemberTagServiceImpl implements MemberTagService {
     private final MemberTagRepository memberTagRepository;
-    private final TagRepository tagRepository;
 
     @Override
     @Transactional
-    public void createMemberTags(Member member, List<Long> tagsIds){    //태그와 사용자 맵핑(연결)
-        for(Long tagId : tagsIds) {
-            Tag tag = tagRepository.findTagById(tagId);
-            Optional<MemberTag> memberTag = memberTagRepository.findById(tagId);
-            if(memberTag.isEmpty()){
-                memberTagRepository.save(MemberTag.newMemberTag(member, tag));
+    public void createMemberTags(Member member, List<Tag> tags){    //태그와 사용자 맵핑(연결)
+        tags.forEach(tag -> {
+                Optional<MemberTag> memberTag = memberTagRepository.findMemberTagByTag(tag);
+                if(memberTag.isEmpty()){
+                    memberTagRepository.save(MemberTag.newMemberTag(member, tag));
+                }
+                else{
+                    memberTag.get().updateUpdatedAt(LocalDateTime.now());
+                }
             }
-            else{
-                memberTag.get().updateUpdatedAt(LocalDateTime.now());
-            }
-        }
+        );
     }
 }
