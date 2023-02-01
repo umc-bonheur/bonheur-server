82f59161edd694513255757de0c058339adb2a9f
Torres-09 <dla612@naver.com>
2023-02-01 20:41:07 +0900

    feat: 파일 업로드 용량 예외 처리
    공통 에러 처리이다.
    각각의 파일제한은 10MB, 전체 Request의 제한은 30MB이다.

--- src/main/java/com/bonheur/config/advice/ControllerExceptionAdvice.java
+++ src/main/java/com/bonheur/config/advice/ControllerExceptionAdvice.java
@@ -1,12 +1,29 @@
 package com.bonheur.config.advice;
 
+import com.bonheur.domain.common.dto.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.tomcat.util.http.fileupload.impl.FileSizeLimitExceededException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.multipart.MaxUploadSizeExceededException;
+
+import static com.bonheur.domain.common.exception.dto.ErrorCode.E400_INVALID_FILE_SIZE_TOO_LARGE;
 
 @Slf4j
 @RequiredArgsConstructor
 @RestControllerAdvice
 public class ControllerExceptionAdvice {
 
+    /**
+     *  최대 허용한 파일 크기 초과하는 경우 발생하는 Exception
+     */
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MaxUploadSizeExceededException.class)
+    private ApiResponse<Object> handleMaxUploadSizeExceededException(MaxUploadSizeExceededException e) {
+        log.error(e.getMessage(), e);
+        return ApiResponse.error(E400_INVALID_FILE_SIZE_TOO_LARGE);
+    }
 }
