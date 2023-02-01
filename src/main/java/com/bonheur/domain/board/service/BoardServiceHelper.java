2050f8da28b235df2eec4a92bdcaa7388bea4a02
Torres-09 <dla612@naver.com>
2023-02-01 22:12:03 +0900

    feat: Helper 클래스 생성

---
+++ src/main/java/com/bonheur/domain/board/service/BoardServiceHelper.java
@@ -0,0 +1,8 @@
+package com.bonheur.domain.board.service;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class BoardServiceHelper {
+}
---
+++ src/main/java/com/bonheur/domain/tag/service/TagServiceHelper.java
@@ -0,0 +1,8 @@
+package com.bonheur.domain.tag.service;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class TagServiceHelper {
+}
