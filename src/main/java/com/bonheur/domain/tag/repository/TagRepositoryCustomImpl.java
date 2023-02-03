e78ae49b96f7f0fac6199045b7e507d3c8903606
m2nja201 <m2nja201@gmail.com>
2023-02-04 02:23:09 +0900

    feature : TagName으로 TagId 조회 API 쿼리 작성

--- src/main/java/com/bonheur/domain/tag/repository/TagRepositoryCustomImpl.java
+++ src/main/java/com/bonheur/domain/tag/repository/TagRepositoryCustomImpl.java
@@ -3,7 +3,21 @@ package com.bonheur.domain.tag.repository;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 
+import static com.bonheur.domain.tag.model.QTag.tag;
+import static com.bonheur.domain.membertag.model.QMemberTag.memberTag;
+
 @RequiredArgsConstructor
 public class TagRepositoryCustomImpl implements TagRepositoryCustom {
     private final JPAQueryFactory queryFactory;
+
+    @Override
+    public Long findOwnTagByTagName(Long memberId, String tagName) {
+        return queryFactory.select(tag.id).from(tag)
+                .where(
+                        // member에 해당되는 태그
+                        memberTag.member.id.eq(memberId),
+                        // tagName에 해당되는 태그
+                        tag.name.eq(tagName)
+                ).join(memberTag).fetchOne();
+    }
 }
