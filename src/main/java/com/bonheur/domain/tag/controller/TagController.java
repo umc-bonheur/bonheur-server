8a8bca1770179ec99f51b2cb0dc079e67da63f65
m2nja201 <m2nja201@gmail.com>
2023-02-04 02:22:42 +0900

    feature : TagName으로 TagId 조회 API controller 구현

--- src/main/java/com/bonheur/domain/tag/controller/TagController.java
+++ src/main/java/com/bonheur/domain/tag/controller/TagController.java
@@ -4,6 +4,7 @@ import com.bonheur.config.swagger.dto.ApiDocumentResponse;
 import com.bonheur.domain.common.dto.ApiResponse;
 import com.bonheur.domain.tag.model.dto.CreateTagRequest;
 import com.bonheur.domain.tag.model.dto.CreateTagResponse;
+import com.bonheur.domain.tag.model.dto.GetTagIdResponse;
 import com.bonheur.domain.tag.service.TagService;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
@@ -23,5 +24,15 @@ public class TagController {
             @Valid @RequestBody CreateTagRequest createTagRequest) {
         Long memberId = 1L; //session 관련 검증 추가해야 함!
         return ApiResponse.success(tagService.createTags(memberId, createTagRequest.getTags()));
+    }
+
+    // # tagName으로 tagId 조회
+    // 회원 인증 어노테이션 추가 필요
+    @ApiDocumentResponse
+    @Operation(summary = "tagName으로 tagId 조회")
+    @GetMapping("/api/tag/{tagName}")
+    public ApiResponse<GetTagIdResponse> getTagIdByTagName(@PathVariable(value = "tagName") @RequestParam String tagName, Long memberId) {
+        GetTagIdResponse getTagIdResponse = tagService.getTagIdByTagName(memberId, tagName);
+        return ApiResponse.success(getTagIdResponse);
     }
 }
