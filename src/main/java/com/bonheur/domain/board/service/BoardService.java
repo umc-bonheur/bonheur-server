bd22efa14a6a28a7dd27d109fd4f88f07b903279
Torres-09 <dla612@naver.com>
2023-02-04 15:24:18 +0900

    feat: 게시물 상세조회 API 수정

--- src/main/java/com/bonheur/domain/board/service/BoardService.java
+++ src/main/java/com/bonheur/domain/board/service/BoardService.java
@@ -10,14 +10,23 @@ import java.time.LocalDate;
 import java.util.List;
 
 public interface BoardService {
-     Slice<GetBoardsResponse> getAllBoards(Long lastBoardId, Long memberId, Pageable pageable);
-     DeleteBoardResponse deleteBoard(Long memberId, Long boardId);
-     Slice<GetBoardsResponse> getBoardsByTag(Long lastBoardId, Long memberId, List<Long> tagIds, Pageable pageable);
-     CreateBoardResponse createBoard(Long memberId, CreateBoardRequest request, List<MultipartFile> images) throws IOException;
-     UpdateBoardResponse updateBoard(Long boardId, UpdateBoardRequest request, List<MultipartFile> images) throws IOException;
-     GetBoardResponse getBoard(Long boardId);
-     GetBoardsGroupsResponse getBoardsGroups(Slice<GetBoardsResponse> getBoardsResponseSlice);
-     List<GetCalendarResponse> getCalendar(Long memberId, int year, int month);
-     Slice<GetBoardsResponse> getBoardsByDate(Long memberId, Long lastBoardId, LocalDate localDate, Pageable pageable);
-     Long getNumOfBoardsByDate(Long memberId, LocalDate localDate);
+    Slice<GetBoardsResponse> getAllBoards(Long lastBoardId, Long memberId, Pageable pageable);
+
+    DeleteBoardResponse deleteBoard(Long memberId, Long boardId);
+
+    Slice<GetBoardsResponse> getBoardsByTag(Long lastBoardId, Long memberId, List<Long> tagIds, Pageable pageable);
+
+    CreateBoardResponse createBoard(Long memberId, CreateBoardRequest request, List<MultipartFile> images) throws IOException;
+
+    UpdateBoardResponse updateBoard(Long boardId, UpdateBoardRequest request, List<MultipartFile> images) throws IOException;
+
+    GetBoardResponse getBoard(Long memberId, Long boardId);
+
+    GetBoardsGroupsResponse getBoardsGroups(Slice<GetBoardsResponse> getBoardsResponseSlice);
+
+    List<GetCalendarResponse> getCalendar(Long memberId, int year, int month);
+
+    Slice<GetBoardsResponse> getBoardsByDate(Long memberId, Long lastBoardId, LocalDate localDate, Pageable pageable);
+
+    Long getNumOfBoardsByDate(Long memberId, LocalDate localDate);
 }
