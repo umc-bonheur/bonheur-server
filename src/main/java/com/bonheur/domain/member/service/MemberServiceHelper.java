7c47a933b107656b95099b8f4f9a935cb2274be5
zer0silver <duddms0_0@naver.com>
2023-02-06 19:21:45 +0900

    feat : member Id로 존재하는 회원인지 검증하는 로직 추가

--- src/main/java/com/bonheur/domain/member/service/MemberServiceHelper.java
+++ src/main/java/com/bonheur/domain/member/service/MemberServiceHelper.java
@@ -1,11 +1,14 @@
 package com.bonheur.domain.member.service;
 
 import com.bonheur.domain.common.exception.ConflictException;
+import com.bonheur.domain.common.exception.NotFoundException;
+import com.bonheur.domain.member.model.Member;
 import com.bonheur.domain.member.model.MemberSocialType;
 import com.bonheur.domain.member.repository.MemberRepository;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
+import static com.bonheur.domain.common.exception.dto.ErrorCode.E404_NOT_EXISTS_MEMBER;
 import static com.bonheur.domain.common.exception.dto.ErrorCode.E409_DUPLICATE_MEMBER;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
@@ -13,6 +16,19 @@ public class MemberServiceHelper {
     static void validateNotExistsUser(MemberRepository memberRepository, String socialId, MemberSocialType socialType) {
         if (memberRepository.existMemberBySocialInfo(socialId, socialType)) {
             throw new ConflictException(String.format("이미 가입한 회원(%s - %s) 입니다", socialId, socialType), E409_DUPLICATE_MEMBER);
+        }
+    }
+
+    static Member getExistMember(MemberRepository memberRepository, Long memberId) {
+        if (!memberRepository.existsById(memberId)){
+            throw new NotFoundException("해당 id를 가진 회원이 존재하지 않습니다.", E404_NOT_EXISTS_MEMBER);
+        }
+        return memberRepository.findMemberById(memberId);
+    }
+
+    static void validateMemberExists(MemberRepository memberRepository, Long memberId) {
+        if (!memberRepository.existsById(memberId)){
+            throw new NotFoundException("해당 id를 가진 회원이 존재하지 않습니다.", E404_NOT_EXISTS_MEMBER);
         }
     }
 }
