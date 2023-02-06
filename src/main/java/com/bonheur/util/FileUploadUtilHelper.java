cc25dbe081347ad007b1598220f9823211a82b1f
zer0silver <duddms0_0@naver.com>
2023-02-06 23:33:28 +0900

    fix: multipartFile.getContentType()으로 파일 확장자 추출 - 기존 파일명을 인덱싱 하여 확장자를 추출하는 방식에서 getContentType()을 활용하는 방식으로 수정하였습니다.

--- src/main/java/com/bonheur/util/FileUploadUtilHelper.java
+++ src/main/java/com/bonheur/util/FileUploadUtilHelper.java
@@ -1,13 +1,10 @@
 package com.bonheur.util;
 
 import com.bonheur.domain.common.exception.InvalidException;
-import com.bonheur.domain.common.exception.NotFoundException;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
-import org.springframework.web.multipart.MultipartFile;
 
 import static com.bonheur.domain.common.exception.dto.ErrorCode.E400_INVALID_UPLOAD_FILE_EXTENSION;
-import static com.bonheur.domain.common.exception.dto.ErrorCode.E400_MISSING_FILE;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class FileUploadUtilHelper {
@@ -17,5 +14,4 @@ public class FileUploadUtilHelper {
             throw new InvalidException("파일 확장자를 확인해주세요.", E400_INVALID_UPLOAD_FILE_EXTENSION);
         }
     }
-
 }
