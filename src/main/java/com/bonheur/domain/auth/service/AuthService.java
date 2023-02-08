9fc67437b3c2808358332390b8c21dc300ba0bd9
Torres-09 <dla612@naver.com>
2023-02-08 15:00:41 +0900

    feat: 회원 가입 API 프로필 이미지 업로드 추가

--- src/main/java/com/bonheur/domain/auth/service/AuthService.java
+++ src/main/java/com/bonheur/domain/auth/service/AuthService.java
@@ -2,9 +2,12 @@ package com.bonheur.domain.auth.service;
 
 import com.bonheur.domain.auth.model.dto.LoginRequest;
 import com.bonheur.domain.auth.model.dto.SocialSignUpRequest;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 public interface AuthService {
-    Long signUp(SocialSignUpRequest request);
+    Long signUp(SocialSignUpRequest request, MultipartFile profileImage) throws IOException;
 
     Long login(LoginRequest request);
 
