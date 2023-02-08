51754cdbfe46cbdede8a9f8777920d70631ce565
zer0silver <duddms0_0@naver.com>
2023-02-08 19:22:46 +0900

    feat: tagIds에 @NotBlank 추가

--- src/main/java/com/bonheur/domain/board/model/dto/GetBoardByTagRequest.java
+++ src/main/java/com/bonheur/domain/board/model/dto/GetBoardByTagRequest.java
@@ -4,10 +4,12 @@ import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
 import java.util.List;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class GetBoardByTagRequest {
+    @NotBlank
     private List<Long> tagIds;
 }
