2c4d23be6a160a70b4397da71caef7206f3c9882
Torres-09 <dla612@naver.com>
2023-02-09 21:42:11 +0900

    feat: 게시물 반환 시 예외처리 추가

--- src/main/java/com/bonheur/domain/board/service/BoardServiceHelper.java
+++ src/main/java/com/bonheur/domain/board/service/BoardServiceHelper.java
@@ -1,8 +1,23 @@
 package com.bonheur.domain.board.service;
 
+import com.bonheur.domain.board.model.Board;
+import com.bonheur.domain.board.repository.BoardRepository;
+import com.bonheur.domain.common.exception.NotFoundException;
+import com.bonheur.domain.common.exception.UnAuthorizedException;
+import com.bonheur.domain.common.exception.dto.ErrorCode;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class BoardServiceHelper {
+    public static Board findBoardByMemberId(BoardRepository boardRepository, Long memberId, Long boardId) {
+        Board board = boardRepository.findBoardByIdWithMemberAndImages(memberId);
+        if (board == null) {
+            throw new NotFoundException(String.format("해당하는 게시물(%s)는 존재하지 않습니다.", boardId), ErrorCode.E404_NOT_EXISTS);
+        }
+        if (!board.getMember().getId().equals(memberId)) {
+            throw new UnAuthorizedException(String.format("해당 회원(%s)에게 권한이 없습니다.", memberId));
+        }
+        return board;
+    }
 }
