877a2b54bf0263b9d9ab57830db5bbd9ef98d669
m2nja201 <m2nja201@gmail.com>
2023-02-11 00:29:04 +0900

    Fix : 게시글 태그별 조회 파라미터 변경 - 각 RequestParam, RequestBody를 합침

--- src/main/java/com/bonheur/domain/board/model/dto/GetBoardByTagRequest.java
+++ src/main/java/com/bonheur/domain/board/model/dto/GetBoardByTagRequest.java
@@ -10,6 +10,8 @@ import java.util.List;
 @Getter
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class GetBoardByTagRequest {
+    private GetBoardsRequest request;
+
     @NotEmpty(message = "태그 아이디를 입력해주세요.")
     private List<Long> tagIds;
 }
