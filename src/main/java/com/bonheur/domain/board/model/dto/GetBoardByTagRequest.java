b9ae8b77d4c0f927c67b034f96707f310ce6561c
m2nja201 <m2nja201@gmail.com>
2023-02-12 18:22:25 +0900

    Fix : [게시글 태그별 조회] DTO 통합

--- src/main/java/com/bonheur/domain/board/model/dto/GetBoardByTagRequest.java
+++ src/main/java/com/bonheur/domain/board/model/dto/GetBoardByTagRequest.java
@@ -3,6 +3,7 @@ package com.bonheur.domain.board.model.dto;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import reactor.util.annotation.Nullable;
 
 import javax.validation.constraints.NotEmpty;
 import java.util.List;
@@ -10,7 +11,11 @@ import java.util.List;
 @Getter
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class GetBoardByTagRequest {
-    private GetBoardsRequest request;
+    @Nullable
+    private String orderType;
+
+    @Nullable
+    private Long lastBoardId;
 
     @NotEmpty(message = "태그 아이디를 입력해주세요.")
     private List<Long> tagIds;
