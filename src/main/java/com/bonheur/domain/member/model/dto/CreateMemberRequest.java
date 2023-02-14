f5206cc72e2057e708dcd1af52c3cba3c2024d3e
Torres-09 <dla612@naver.com>
2023-02-14 18:13:35 +0900

    feat: 닉네임 Validation 추가

--- src/main/java/com/bonheur/domain/member/model/dto/CreateMemberRequest.java
+++ src/main/java/com/bonheur/domain/member/model/dto/CreateMemberRequest.java
@@ -1,9 +1,11 @@
 package com.bonheur.domain.member.model.dto;
 
+import com.bonheur.config.validator.NickName;
 import com.bonheur.domain.file.dto.FileUploadResponse;
 import com.bonheur.domain.member.model.Member;
 import com.bonheur.domain.member.model.MemberSocialType;
 import lombok.*;
+import org.hibernate.validator.constraints.Length;
 
 import javax.validation.constraints.NotNull;
 
@@ -17,6 +19,8 @@ public class CreateMemberRequest {
     private MemberSocialType socialType;
 
     // 닉네임 조건 체크 어노테이션 이후 추가
+    @Length(max = 7)
+    @NickName
     private String nickname;
 
     @Builder(access = AccessLevel.PRIVATE)
